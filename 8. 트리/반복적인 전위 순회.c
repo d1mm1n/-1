2462222275338b4dc77c845d86ad170876330d75
d1mm1n <dimmining@naver.com>
2023-05-28 22:04:47 +0900

    반복적인 전위 순회

---
+++ 8. 트리/반복적인 전위 순회.c
@@ -0,0 +1,70 @@
+//반복적인 전위 순회
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+typedef struct TreeNode {
+	char data[100];
+	struct TreeNode* left, * right;
+}TreeNode;
+
+#define SIZE 100
+int top = -1;
+TreeNode* stack[SIZE];
+
+void push(TreeNode* p) {
+	if (top < SIZE - 1) {
+		stack[++top] = p;
+	}
+}
+
+TreeNode* pop() {
+	TreeNode* p = NULL;
+	if (top >= 0) {
+		p = stack[top--];
+	}
+	return p;
+}
+
+void inorder_iter(TreeNode *root) {
+	if (root == NULL)
+		return;
+
+	push(root);
+
+	while (top >= 0) {
+		TreeNode* node = pop();
+		printf("[%s] ", node->data);
+
+		if (node->right != NULL)
+			push(node->right);
+
+		if (node->left != NULL)
+			push(node->left);
+	}
+}
+//          A
+//    B          C
+//  D   E      F   G
+//        H       I
+
+
+TreeNode n1 = { "H",NULL,NULL};
+TreeNode n2 = { "I",NULL,NULL };
+TreeNode n3 = { "G",&n2,NULL };
+TreeNode n4 = { "F",NULL,NULL };
+TreeNode n5 = { "D",NULL,NULL };
+TreeNode n6 = { "E",NULL,&n1};
+TreeNode n7 = { "B",&n5,&n6 };
+TreeNode n8 = { "C",&n4,&n3 };
+TreeNode n9 = { "A",&n7,&n8 };
+TreeNode* root = &n9;
+
+
+int main(void) {
+	printf("전위 순회=");
+	inorder_iter(root);
+	printf("\n");
+	return 0;
+}
